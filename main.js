1668ff706927d1c15643b687a033300aeef17d7a
Jeremie Elbaz <jeremie-e@hotmail.fr>
2017-05-02 15:43:36 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path');
+const url = require('url');
+
+require('dotenv').config();
+
+let win = null;
+
+app.on('ready', function () {
+
+  // Initialize the window to our specified dimensions
+  win = new BrowserWindow({
+    // skipTaskbar: true,
+    // autoHideMenuBar: true,
+    width: 480, height: 320});
+
+  // Specify entry point
+  if (process.env.PACKAGE === 'true'){
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'dist/index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+    win.webContents.openDevTools();
+  } else {
+    win.loadURL(process.env.HOST);
+    win.webContents.openDevTools();
+  }
+
+  // Remove window once app is closed
+  win.on('closed', function () {
+    win = null;
+  });
+
+});
+
+// app.on('activate', () => {
+//   if (win === null) {
+//     createWindow()
+//   }
+// });
+//
+// app.on('window-all-closed', function () {
+//   if (process.platform != 'darwin') {
+//     app.quit();
+//   }
+// });
