9cdd7bb8b0e4952e7166ae6a669ec61216d26c80
Jeremie Elbaz <jeremie-e@hotmail.fr>
2017-05-03 22:03:48 +0200

    delete

--- src/app/inscription.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './inscription.component.html',
-})
-export class InscriptionComponent {
-  // title = 'app works!';
-}
